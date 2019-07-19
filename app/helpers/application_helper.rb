30f540ba7c847ddd51f9ef1985cf52285b16a63a
Florian Uphoff <florianuphoff@secure.mailbox.org>
2019-07-19 12:35:59 +0200

    adds ror project with webpack integration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def webpack_css(*args)
+    urls = Array(args).map do |style|
+      webpack_fingerprint_for("#{style}.css")
+    end
+
+    stylesheet_link_tag(*urls, media: nil)
+  end
+
+  def webpack_js(*args)
+    urls = Array(args).map do |js|
+      webpack_fingerprint_for("#{js}.js")
+    end
+
+    javascript_include_tag(*urls)
+  end
+
+  def webpack_fingerprint_for(name)
+    @chunk_manifest ||= JSON.parse(File.read('public/packs/manifest.json'))
+
+    if @chunk_manifest[name].nil?
+      return ''
+    end
+
+    @chunk_manifest[name]
+  end
 end
